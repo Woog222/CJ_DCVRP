7ff2559471205a683651abd259181a224717e4f7
jewook <gjwpdnr0918@gmail.com>
2023-07-09 23:10:05 +0900

    initial structure & data loading logic

--- object/graph.cpp
+++ object/graph.cpp
@@ -1,7 +1,3 @@
-//
-// Created by achui on 2023-07-09.
-//
-
 #include "graph.h"
 #include <iostream>
 #include <fstream>
@@ -9,10 +5,6 @@
 Graph::Graph(const string& file_dir){
 
     ifstream fs(file_dir);
-    if (fs.fail()) {
-        cerr << ("invalid directories : " + file_dir) << endl;
-        exit(1);
-    }
 
     int idx = 0;
     while (fs.good()) {
@@ -28,5 +20,15 @@ Graph::Graph(const string& file_dir){
         adj[from].push_back(
                 pair<int, edge>(to, edge{length, time})
         );
+    }
+
+    // table reset with {-1,-1}
+    edge init_value = {-1.0,-1.0};
+    table.resize(idx, vector<edge>(idx, init_value));
+
+    for (int i =0; i<idx; ++i) {
+        for (const auto& e : adj[i]) {
+            table[i][e.first] = e.second;
+        }
     }
 }
--- object/graph.h
+++ object/graph.h
@@ -6,15 +6,14 @@
 #include "config.h"
 #include <map>
 
+typedef pair<double, double> edge;
 
 class Graph {
-    struct edge{
-        double length, time;
-    };
 
 public:
     map<string, int> IDX;
     vector<pair<int,edge>> adj[GRAPH_SIZE];
+    vector<vector<edge>> table; // graph matrix
 
     Graph(const string& file_dir);
 };
